611ca6daa7255f123564c82e843ce98958b3eb30
Htocki <pasztownia@gmail.com>
2020-11-03 18:03:11 +0300

    A program for matrix multiplication has been implemented.

---
+++ matrix multiplication/code.py
@@ -0,0 +1,20 @@
+import numpy
+import random
+
+print("Enter dimension: ")
+count = int(input())
+lower_bound = 0
+upper_bound = 9
+
+A = numpy.matrix(
+    [[random.randint(lower_bound, upper_bound) for i in range(count)]
+    for j in range(count)])
+print("\nMatrix A:\n", A)
+
+B = numpy.matrix(
+    [[random.randint(lower_bound, upper_bound) for i in range(count)]
+    for j in range(count)])
+print("\nMatrix B:\n", B)
+
+C = A.dot(B)
+print("\nMatrix C:\n", C)
